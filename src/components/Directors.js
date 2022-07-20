ad0721dc05adfc9b37b46e63dbd48bd0466e7be9
Edsey omenda <omendajames0317@gmail.com>
2022-07-20 23:46:47 +0300

    completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { directors } from "../data";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+function Directors () {
+  const myDirectors = directors.map((director) => (
+    <div key={director.name}>
+      <h2>Name :{director.name}</h2>
+      Movies:
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
 
-export default Directors;
+  return (
+    <div>{/*{code here}*/}
+      <h1>Directors Page</h1>
+      {myDirectors}
+    </div>
+  );
+};
+
+export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+function Movies(){
+  const myMovies = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>Name :{movie.title}</h2>
+      Time: {movie.time} minutes<br></br>
+      Genres:
+      <ul>
+        
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {myMovies}
+    </div>
+  );
+};
 
 export default Movies;
